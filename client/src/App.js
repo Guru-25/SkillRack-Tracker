154b8fa631cb631a620748923f00a2acf742ebab
Guru <mail@gururaja.in>
2024-12-24 01:58:03 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -67,7 +67,7 @@ const getGreeting = (name) => {
     greeting = 'Happy late night';
   }
   
-  return `${greeting}, ${capitalizedName} 😊`;
+  return `🎄 ${greeting}, ${capitalizedName} 😊`;
 }
 
 const ReleaseNote = () => {
